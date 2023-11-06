5afc8c85a5d119185400d66160d4f8929d5f90b8
taraassov <taraassov03@gmail.com>
2023-11-06 14:38:38 +0500

    views.py

---
+++ tutorial/views.py
@@ -0,0 +1,76 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, generics
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import IsAuthenticated
+
+from tutorial.models import Course, Lesson, Payment
+from tutorial.permissions import IsNotStaffUser, IsOwnerOrStaff
+from tutorial.serializers import CourseSerializer, LessonSerializer, PaymentSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+    def get_permissions(self):
+        if self.action == "create":
+            permission_classes = [IsAuthenticated, IsNotStaffUser]
+        elif self.action in ["update", "partial_update"]:
+            permission_classes = [IsAuthenticated, IsOwnerOrStaff]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+    def perform_create(self, serializer):
+        new_course = serializer.save()
+        new_course.owner = self.request.user
+        new_course.save()
+
+    def get_queryset(self):
+        if not self.request.user.is_staff:
+            return Course.objects.filter(owner=self.request.user)
+        elif self.request.user.is_staff:
+            return Course.objects.all()
+        else:
+            raise PermissionDenied
+
+
+class LessonCreateAPIView(generics.CreateAPIView):
+    serializer_class = LessonSerializer
+    permission_classes = [IsNotStaffUser]
+
+    def perform_create(self, serializer):
+        new_lesson = serializer.save()
+        new_lesson.owner = self.request.user
+        new_lesson.save()
+
+
+class LessonListAPIView(generics.ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+    permission_classes = [IsOwnerOrStaff]
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+    permission_classes = [IsOwnerOrStaff]
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
+    permission_classes = [IsNotStaffUser]
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('course', 'lesson', 'payment_method')
+    ordering_fields = ('-date',)
