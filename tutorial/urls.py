50b3947d3d5843277b42ba3fc28ddb6d521938eb
taraassov <taraassov03@gmail.com>
2023-11-06 14:38:09 +0500

    urls.py

---
+++ tutorial/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+
+from tutorial.apps import TutorialConfig
+from rest_framework.routers import DefaultRouter
+
+from tutorial.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView, PaymentListAPIView
+
+app_name = TutorialConfig.name
+
+router = DefaultRouter()
+router.register(r'course', CourseViewSet, basename='course')
+
+urlpatterns = [
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-get'),
+    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
+
+    # payment
+    path('payment/', PaymentListAPIView.as_view(), name='payment-list'),
+] + router.urls
